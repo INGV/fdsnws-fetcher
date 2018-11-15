9fb1ac72de2d9a22af9f5a04356d62fdce8ec1bb
Valentino Lauciani <valentino.lauciani@ingv.it>
2018-11-15 17:31:58 +0100

    Initial commit

---
+++ 021_get_dless-resp-parallel.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+#
+
+#
+# xml2seed.sh
+#
+# This script helps staging of station response metadata to a local storage.
+# It uses an FDSN station service to download station metadata in StationXML
+# and converts them in the desired format, here RESP files.
+#
+# (c) 2017 Peter Danecek <peter.danecek@ingv.it>,
+#          Valentino Lauciani <valentino.lauciani@ingv.it>,
+#          Matteo Quintiliani <matteo.quintiliani@ingv.it>,
+#          Istituto Nazione di Geofisica e Vulcanologia.
+# 
+#####################################################3
+
+# Import config file
+. $(dirname $0)/config.sh
+
+# Check DIR_TMP
+if [ ! -d ${DIR_TMP} ]; then
+        echo ""
+        echo " DIR_TMP doesn't exists."
+        echo ""
+        exit 1
+fi
+
+### START - Check parameters ###
+TYPE=
+while getopts :o:u:t: OPTION
+do
+	case ${OPTION} in
+        o)	FILE_OUTPUT_DLESS="${OPTARG}"
+			;;
+        u)	STATIONXML_INPUT_URL="${OPTARG}"
+			;;
+        t)	TYPE="${OPTARG}"
+			;;
+        \?) echo "Invalid option: -$OPTARG" >/dev/null
+			shift
+			;;
+        *)  #echo $OPTARG >/dev/null
+			echo "Invalid OPTARG: -$OPTARG" >&2
+            ;;
+	esac
+done
+### END - Check parameters ###
+
+# Set var
+BASENAME_DLESS=$( basename ${FILE_OUTPUT_DLESS} )
+DIRNAME_DLESS=$( dirname ${FILE_OUTPUT_DLESS} )
+DIR_NODE=$( cd ${DIRNAME_DLESS} && cd ../ && pwd )
+DIR_DLESS_LOG_NODE=${DIR_NODE}/log
+
+
+# create dir
+if [ ! -d ${DIR_DLESS_LOG_NODE} ]; then
+    mkdir -p ${DIR_DLESS_LOG_NODE}
+fi
+
+echo " create DLESS \"${BASENAME_DLESS}\" from \"${STATIONXML_INPUT_URL}\""
+${STATIONXML_TO_SEED} -o ${FILE_OUTPUT_DLESS} "${STATIONXML_INPUT_URL}" >> ${DIR_DLESS_LOG_NODE}/${BASENAME_DLESS}.stationxml-converter.out 2>> ${DIR_DLESS_LOG_NODE}/${BASENAME_DLESS}.stationxml-converter.err
+RET_STATIONXML_TO_SEED=${?}
+if (( ${RET_STATIONXML_TO_SEED} != 0 )); then
+    echo "  ERROR - Retriving StationXML from \"${STATIONXML_INPUT_URL}\". Check: ${DIR_DLESS_LOG_NODE}/${BASENAME_DLESS}.stationxml-converter.err"
+    #cat ${DIR_DLESS_LOG_NODE}/${BASENAME_DLESS}.stationxml-converter.err
+    echo ""
+fi
+
+# Create RESP if requested
+if [[ "${TYPE}" == "resp" ]]; then
+    if [ -s ${FILE_OUTPUT_DLESS} ]; then
+        DIR_RESP_NODE=${DIR_NODE}/resp
+        DIR_RESP_LOG_NODE=${DIR_RESP_NODE}/log
+        if [ ! -d ${DIR_RESP_NODE} ]; then
+            mkdir -p ${DIR_RESP_NODE}
+        fi
+        if [ ! -d ${DIR_RESP_LOG_NODE} ]; then
+            mkdir -p ${DIR_RESP_LOG_NODE}
+        fi
+        echo " create RESP from \"${FILE_OUTPUT_DLESS}\""
+        ${RDSEED} -f ${FILE_OUTPUT_DLESS} -q ${DIR_RESP_NODE} >> ${DIR_RESP_LOG_NODE}/${BASENAME_DLESS}.rdseed.out 2>> ${DIR_RESP_LOG_NODE}/${BASENAME_DLESS}.rdseed.err
+        RET_RDSEED=${?}
+        if (( ${RET_RDSEED} != 0 )); then
+            cat ${DIR_RESP_LOG_NODE}/${BASENAME_DLESS}.rdseed.err
+            echo -e "\n"
+        fi
+#    else
+#        echo "  the DLESS \"${FILE_OUTPUT_DLESS}\" doesn't exist"
+    fi
+fi
